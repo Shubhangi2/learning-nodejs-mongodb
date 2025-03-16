e9de9b4ee1d6a21bec8ef2b9ad9ca4131351c5f7
Shubhangi Jadhav <jshubhangi958@gmail.com>
2025-03-16 17:41:02 +0530

    utils classes added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDb();
+connectDb()
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  });
 
 /*
 (async () => {
